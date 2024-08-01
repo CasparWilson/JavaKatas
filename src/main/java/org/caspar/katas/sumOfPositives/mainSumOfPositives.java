48ff9eb5e64849db5f1f5c305a8233cb4fda940e
CasparWilson <casparwilson111@gmail.com>
2024-08-01 09:57:22 +0100

    kata finished with tests

--- src/main/java/org/caspar/katas/sumOfPositives/mainSumOfPositives.java
+++ src/main/java/org/caspar/katas/sumOfPositives/mainSumOfPositives.java
@@ -1,4 +1,13 @@
 package org.caspar.katas.sumOfPositives;
 
 public class mainSumOfPositives {
+    public static int sumOfPositives(int[] numbers) {
+        int sum = 0;
+        for (int i = 0; i < numbers.length; i++) {
+            if (numbers[i] > 0) {
+                sum += numbers[i];
+            }
+        }
+        return sum;
+    }
 }
--- src/test/java/org/caspar/katas/sumOfPositives/sumOfPositivesTest.java
+++ src/test/java/org/caspar/katas/sumOfPositives/sumOfPositivesTest.java
@@ -1,7 +1,28 @@
 package org.caspar.katas.sumOfPositives;
 
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class sumOfPositivesTest {
+    @Test
+    void return0ifPassedEmptyArray() {
+        int[] inputArray = new int[0];
+        assertEquals(0, mainSumOfPositives.sumOfPositives(inputArray));
+    }
+
+    @Test
+    void return0ifPassedArrayOfNegNumbers() {
+        int[] inputArray = new int[] { -1, -2, -3, -4, -5 };
+        assertEquals(0, mainSumOfPositives.sumOfPositives(inputArray));
+    }
+
+    @Test
+    void returnExpectedWhenPassedArrayOfPositiveInts() {
+        int[] inputArray = new int[] { 1, 2, 3, 4, 5 };
+        int expectedSum = 15;
+        assertEquals(expectedSum, mainSumOfPositives.sumOfPositives(inputArray));
+    }
+
 
 }
